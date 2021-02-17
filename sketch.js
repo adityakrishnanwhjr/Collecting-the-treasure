da4ddd4932edac79ef7a6274b0e18ba1be572976
adityakrishnanwhjr <78741696+adityakrishnanwhjr@users.noreply.github.com>
2021-02-17 08:09:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ var path,boy,cash,diamonds,jwellery,sword;
 var pathImg,boyImg,cashImg,diamondsImg,jwelleryImg,swordImg,endImg;
 var treasureCollection = 0;
 var cashG,diamondsG,jwelleryG,swordGroup;
+var obstacle1,obstacle2,obstacle3,obstaclesGroup;
+var obstacle1Img,obstacle2Img,obstacle3Img;
+var deaths=0;
 var PLAY=1;
 var END=0;
 var gameState=1;
@@ -14,6 +17,9 @@ function preload(){
   jwelleryImg = loadImage("jwell.png");
   swordImg = loadImage("sword.png");
   endImg =loadAnimation("gameOver.png");
+  obstacle1Img=loadImage("obstacle1.png");
+  obstacle2Img=loadImage("obstacle2.png");
+  obstacle3Img=loadImage("obstacle3.png");
 }
 
 function setup(){
@@ -35,6 +41,7 @@ cashG=new Group();
 diamondsG=new Group();
 jwelleryG=new Group();
 swordGroup=new Group();
+obstaclesGroup=new Group();
 
 }
 
@@ -56,6 +63,7 @@ function draw() {
     createDiamonds();
     createJwellery();
     createSword();
+    spawnObstacles();
 
     if (cashG.isTouching(boy)) {
       cashG.destroyEach();
@@ -69,9 +77,13 @@ function draw() {
       treasureCollection=treasureCollection+150;
     }else{
       if(swordGroup.isTouching(boy)) {
-        gameState=END;
+        deaths=deaths+1;
         swordGroup.destroyEach();
     }
+  }
+  if(deaths===5){
+    gameState=END;
+    swordGroup.destroyEach();
   }
 
   if(gameState===END){
@@ -91,6 +103,9 @@ function draw() {
     
     swordGroup.destroyEach();
     swordGroup.setVelocityYEach(0);
+    
+    obstaclesGroup.destroyEach();
+    obstaclesGroup.setVelocityYEach(0);
   }
   
   drawSprites();
@@ -142,4 +157,32 @@ function createSword(){
   sword.lifetime = 150;
   swordGroup.add(sword);
   }
+}
+
+function spawnObstacles(){
+  var obstacles=Math.round(random(1,3));
+  if(frameCount%160===0){
+  if(obstacles===1){
+    obstacle1=createSprite(Math.round(random(50,350)),40,10,10);
+    obstacle1.addImage(obstacle1Img);
+    obstacle1.scale=0.1;
+    obstacle1.velocityY=3;
+    obstacle1.lifetime=150;
+    obstaclesGroup.add(obstacle1);
+  }else if(obstacles===2){
+    obstacle2=createSprite(Math.round(random(50,350)),40,10,10);
+    obstacle2.addImage(obstacle2Img);
+    obstacle2.scale=0.1;
+    obstacle2.velocityY=3;
+    obstacle2.lifetime=150;
+    obstaclesGroup.add(obstacle2);
+  }else{
+    obstacle3=createSprite(Math.round(random(50,350)),40,10,10);
+    obstacle3.addImage(obstacle3Img);
+    obstacle3.scale=0.1;
+    obstacle3.velocityY=3;
+    obstacle3.lifetime=150;
+    obstaclesGroup.add(obstacle3);
+  }
+}
 }
